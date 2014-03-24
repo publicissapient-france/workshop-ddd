57f97a6db9cf00339889f6b1cebb7bea9f3c879b
njozwiak <njozwiak@xebia.fr>
2014-03-24 21:55:55 +0100

    Add tests

--- financial/src/main/java/com/xebia/domain/credit/CreditId.java
+++ financial/src/main/java/com/xebia/domain/credit/CreditId.java
@@ -10,51 +10,50 @@ import static com.google.common.base.Strings.isNullOrEmpty;
 
 public final class CreditId extends Identity {
 
-  private String referenceCode;
+    private String referenceCode;
 
-  public CreditId(String id, String referenceCode) {
-    super(id);
-    this.referenceCode = referenceCode;
-  }
-
-  protected CreditId() {
-    super();
-  }
-
-  public String getReferenceCode() {
-    return referenceCode;
-  }
-
-  private void setReferenceCode(String referenceCode) {
-    checkArgument(isNullOrEmpty(referenceCode), "Reference code cannot be null or empty");
-
-    this.referenceCode = referenceCode;
-  }
-
-  @Override
-  protected void validateId(String id) {
-    try {
-      UUID.fromString(id);
-    } catch (Exception e) {
-      throw new IllegalArgumentException("The id has an invalid format.");
-    }
-  }
-
-  @Override
-  public boolean equals(Object object) {
-    boolean equalObjects = false;
-
-    if (object != null && this.getClass() == object.getClass()) {
-      CreditId typedObject = (CreditId) object;
-      equalObjects = this.referenceCode.equals(typedObject.referenceCode);
+    public CreditId(String id, String referenceCode) {
+        super(id);
+        this.setReferenceCode(referenceCode);
     }
 
-    return equalObjects;
-  }
+    protected CreditId() {
+        super();
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(referenceCode);
-  }
+    public String getReferenceCode() {
+        return referenceCode;
+    }
 
+    private void setReferenceCode(String referenceCode) {
+        checkArgument(!isNullOrEmpty(referenceCode), "Reference code cannot be null or empty");
+
+        this.referenceCode = referenceCode;
+    }
+
+    @Override
+    protected void validateId(String id) {
+        try {
+            UUID.fromString(id);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("The id has an invalid format.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        boolean equalObjects = false;
+
+        if (object != null && this.getClass() == object.getClass()) {
+            CreditId typedObject = (CreditId) object;
+            equalObjects = this.referenceCode.equals(typedObject.referenceCode);
+        }
+
+        return equalObjects;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(referenceCode);
+    }
 }
